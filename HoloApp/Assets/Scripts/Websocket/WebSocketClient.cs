cb25ce82090546215bf9c77580487df0e9a7bd54
NguyenRichard <nguyen.richard1997@gmail.com>
2021-07-14 13:14:44 -0400

    Solving crash on label reception and changing pending message to a queue

--- HoloApp/Assets/Scripts/Websocket/WebSocketClient.cs
+++ HoloApp/Assets/Scripts/Websocket/WebSocketClient.cs
@@ -13,21 +13,24 @@ public class WebSocketClient : MonoBehaviour
     [Tooltip("The address of the websocket server")]
     public string WebsocketAddress = "127.0.0.1";
     Camera cam;
-    public GameObject Label;
+    public GameObject labelPrefab;
     private WebSocket ws;
-    private string message = "";
+    private Queue<string> messagesFromServer;
 
     /////////////////////////////
     // Connection to the websocket
     ////////////////////////////
     void Start()
     {
+        messagesFromServer = new Queue<string>();
+
         // initialize camera and websocket
         ws = new WebSocket ($"ws://{WebsocketAddress}");
         cam = Camera.main;
         ws.OnMessage += (sender, e) =>
         {
-            message = e.Data.ToString();
+            string message = e.Data.ToString();
+            messagesFromServer.Enqueue(message);
         };
 
         // connection
@@ -40,46 +43,66 @@ public class WebSocketClient : MonoBehaviour
     ////////////////////////////
     public void getLabels()
     {
-        ws.SendAsync("GetLabels",null);
+        ws.Send("GetLabels");
     }
 
-    /////////////////////////////
-    // Instantiate label with text
-    ////////////////////////////
-    private void InstantiateLabel(GameObject label, Vector3 position, Quaternion rotation, string CloseTxt, string FarTxt, string identifier)
-    {
-        var obj = Instantiate(label, position, rotation);
-        obj.GetComponent<LabelParameters> ().SetParameters(CloseTxt, FarTxt, identifier);
-    }
 
-    /////////////////////////////
-    // Use key input to test sending keyword and receiving coordinates
-    ////////////////////////////
+
     void Update()
     {
+
+        //Use X key on PC platform to send a message to the server requesting labels.
         if (Input.GetKeyUp(KeyCode.X))
         {
             getLabels();
         }
         
-        if (message != "") {
+        //if (message != "") {
 
-            JArray labels = JArray.Parse(message);
+        //Pooling server message and creating a label if a message is received.
+        if(messagesFromServer.Count != 0)
+        {
+            CreateLabel(messagesFromServer.Dequeue());
+        }
+          
+    }
 
+    /// <summary>
+    /// Create a label by parsing the JSON from server and converting information to Unity system.
+    /// </summary>
+    /// <param name="labelsJSON">JSON from server containing all information to instantiate a label</param>
+    public void CreateLabel(string labelsJSON)
+    {
+        if (labelPrefab == null)
+        {
+            Debug.Log("Label prefab cannot be null.");
+            return;
+        }
+        if(labelsJSON == "")
+        {
+            Debug.Log("The JSON is empty");
+            return;
+        }
+
+        JArray labels = JArray.Parse(labelsJSON);
+
+        if(labels != null)
+        {
             foreach (JObject label in labels)
             {
                 if (label == null)
                     return;
 
+                //Get the matrix 4x4 that contains the position and rotation information.
                 Matrix4x4 transform = Matrix4x4.zero;
                 int i = 0;
                 int j = 0;
-                foreach(JArray column in label["position"])
+                foreach (JArray column in label["position"])
                 {
                     if (column == null)
                         return;
 
-                    foreach(float value in column)
+                    foreach (float value in column)
                     {
                         transform[i, j] = value;
                         i++;
@@ -89,28 +112,37 @@ public class WebSocketClient : MonoBehaviour
 
                 }
 
+                //Convert coordinate system.
                 transform = MathUtilities.ConvertOpenGLToUnitySystem(transform);
 
+                //Get position and rotation.
                 Vector3 position = transform.ExtractPosition();
-
-                Debug.Log("pos:"+position);
+                Debug.Log((string)label["info"]["textClose"] + "pos:" + position);
                 Quaternion rotation = transform.ExtractRotation();
-                Debug.Log(rotation.eulerAngles);
+                Debug.Log((string)label["info"]["textClose"] + "rot" + rotation.eulerAngles);
 
-                InstantiateLabel(Label, position, rotation, (string)label["info"]["textClose"], (string)label["info"]["textFar"], (string)label["id"]);
+                InstantiateLabel(labelPrefab, position, rotation, (string)label["info"]["textClose"], (string)label["info"]["textFar"], (string)label["id"]);
             }
+        }
 
-            //OLD_CODE
-            //JObject json = JObject.Parse(message);
-            //Debug.Log("Labels received");
+       
+    }
 
-            //Vector3 position = new Vector3((float)json["position"][0],(float)json["position"][1],(float)json["position"][2]);
-            //Vector3 orientation = new Vector3((float)json["orientation"][0],(float)json["orientation"][1],(float)json["orientation"][2]);
-            //Quaternion rotation = Quaternion.LookRotation(orientation, new Vector3(0,1,0));
+    /////////////////////////////
+    // Instantiate label with text
+    ////////////////////////////
+    private void InstantiateLabel(GameObject label, Vector3 position, Quaternion rotation, string CloseTxt, string FarTxt, string identifier)
+    {
+        if (label == null)
+        {
+            Debug.Log("Label prefab cannot be null.");
+            return;
+        }
 
-            //InstantiateLabel(Label, position, rotation, (string)json["close"], (string)json["far"], (long)json["id"]);
-
-            message = "";
+        var obj = Instantiate(label, position, rotation);
+        if (obj != null)
+        {
+            obj.GetComponent<LabelParameters>()?.SetParameters(CloseTxt, FarTxt, identifier);
         }
     }
 }
